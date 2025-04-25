aecec4c9263a5b562436518c661c40c49d835e0d
Daniel-Brennan <40654455@live.napier.ac.uk>
2025-04-25 22:36:58 +0100

    HTML, CSS & JS for my geography quiz + some minor image renames

---
+++ geographyQuiz.js
@@ -0,0 +1,68 @@
+function RevealAnswerVat(Img, Text, btn, Above, event){
+  let image = document.getElementById(Img);
+  let paragraph = document.getElementById(Text);
+  let button = document.getElementById(btn);
+  let ab =document.getElementById(Above);
+  image.src = 'VaticanCity.jpeg'
+  paragraph.style.marginBottom = '0%'
+  paragraph.style.visibility = 'visible'
+  button.style.visibility = 'hidden';
+  ab.style.visibility = 'visible';
+  button.style.margin = '-4%'
+}
+//two
+function RevealAnswerDen(Img, Text, btn, Above, event){
+  let image = document.getElementById(Img);
+  let paragraph = document.getElementById(Text);
+  let button = document.getElementById(btn);
+  let ab =document.getElementById(Above);
+  image.src = 'danish_Flag.jpg'
+  paragraph.style.marginBottom = '0%'
+  paragraph.style.visibility = 'visible'
+  button.style.visibility = 'hidden';
+  ab.style.visibility = 'visible';
+  button.style.margin = '-4%'
+}
+
+//three
+function RevealAnswerindia(Img, Text, btn, Above, event){
+  let image = document.getElementById(Img);
+  let paragraph = document.getElementById(Text);
+  let button = document.getElementById(btn);
+  let ab =document.getElementById(Above);
+  image.src = 'india.jpg'
+  paragraph.style.marginBottom = '0%'
+  paragraph.style.visibility = 'visible'
+  button.style.visibility = 'hidden';
+  ab.style.visibility = 'visible';
+  button.style.margin = '-4%'
+}
+//four
+function RevealAnswerMount(Img, Text, btn, Above, event){
+  let image = document.getElementById(Img);
+  let paragraph = document.getElementById(Text);
+  let button = document.getElementById(btn);
+  let ab =document.getElementById(Above);
+  image.src = 'Mauna_kea.jpg'
+  paragraph.style.marginBottom = '0%'
+  paragraph.style.visibility = 'visible'
+  button.style.visibility = 'hidden';
+  ab.style.visibility = 'visible';
+  button.style.margin = '-4%'
+}
+
+//five
+function RevealAnswerSud(Img, Text, btn, Above, event){
+  let image = document.getElementById(Img);
+  let paragraph = document.getElementById(Text);
+  let button = document.getElementById(btn);
+  let ab =document.getElementById(Above);
+  image.src = 'SouthSudan.png'
+  paragraph.style.marginBottom = '0%'
+  paragraph.style.visibility = 'visible'
+  button.style.visibility = 'hidden';
+  ab.style.visibility = 'visible';
+  button.style.margin = '-4%'
+}
+
+
