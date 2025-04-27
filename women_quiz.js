8191ac79864dc21191ce1ee54af53d3f6d6ac7a7
ChloeAWhelan <40735430@live.napier.ac.uk>
2025-04-27 18:49:57 +0100

    Add files via upload

---
+++ women_quiz.js
@@ -0,0 +1,15 @@
+ function showMessage() {
+      alert(
+        "Quiz Answers:\n\n" +
+        "1. Ada Lovelace\n" +
+        "2. 1903\n" +
+        "3. DNA structure\n" +
+        "4. Althea Gibson\n" +
+        "5. Ruth Bader Ginsburg\n" +
+        "6. Mount Everest\n" +
+        "7. Sally Ride\n" +
+        "8. Atlantic Ocean\n" +
+        "9. Florence Nightingale\n" +
+        "10. The Suffragettes"
+      );
+    }
